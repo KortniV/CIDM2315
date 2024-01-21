542496424bdc239283f1290b1fe569e271aa681e
KortniV <156952914+KortniV@users.noreply.github.com>
2024-01-20 22:11:42 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float X = 2.5f;
+        float Y = 3.3f;
+        float Z = 4*(X*X)+(3*Y);
+        Console.WriteLine($"X = 2.5, Y = 3.3\nThe value of Z is: {Z}");
+    }
+}
