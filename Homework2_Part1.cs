6cbaf79b26eb7f477c675f7b0944eb8db1af4f42
KortniV <156952914+KortniV@users.noreply.github.com>
2024-01-29 23:59:37 -0600

    Add files via upload

---
+++ Homework2_Part1.cs
@@ -0,0 +1,35 @@
+﻿class Homework2_Part1
+{
+static void Main()
+{
+Console.WriteLine("Please Input a Letter Grade: \n");
+string letter_grade = Console.ReadLine();
+switch(letter_grade)
+{
+    case "A":
+        //print the the text for A
+        Console.WriteLine(@"GPA point: 4");
+        break;
+    case "B":
+        //print the the text for B
+        Console.WriteLine(@"GPA point: 3");
+        break;
+    case "C":
+        //print the the text for C
+        Console.WriteLine(@"GPA point: 2");
+        break;
+    case "D":
+        //print the the text for D
+        Console.WriteLine(@"GPA point: 1");
+        break;
+    case "F":
+        //print the the text for F
+        Console.WriteLine(@"GPA point: 0");
+        break;
+    default:
+        //print Wrong Letter Grade!
+        Console.WriteLine(@"Wrong Letter Grade!");
+        break;
+    }
+}
+}
---
+++ Homework2_Part2.cs
@@ -0,0 +1,33 @@
+﻿internal class Homework2_Part2
+{
+    private static void Main()
+{
+
+//number one
+Console.Write("Please input the first num: ");
+int one = Convert.ToInt16(Console.ReadLine());
+
+//number two
+Console.Write("Please input the second num: ");
+int two= Convert.ToInt16(Console.ReadLine());
+
+//number three
+Console.Write("Please input the third num: ");
+int three = Convert.ToInt16(Console.ReadLine());
+
+//comparison using if statements
+if (one < two && one < three)
+{
+    Console.Write($"The smallest value is {one}");
+}
+else if (two < one && two < three)
+{
+    Console.Write($"The smallest value is {two}");
+}
+else
+{
+    Console.Write($"The smallest value is {three}");
+}
+
+}
+}
